7ec2ac43e4bf8c7c681033af9302e77169d77947
Raphael Freudiger <laser_b@gmx.ch>
2017-09-15 17:57:11 +0200

    chore: port functions from mindmade tools

--- teleboy.py
+++ teleboy.py
@@ -5,12 +5,14 @@ import base64
 import cookielib
 import urllib
 import urllib2
+import HTMLParser
+import re
 import xbmc
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
-from mindmade import fetchHttp, log, notify, htmldecode, sayHi
 import simplejson
+import datetime
 
 __author__ = "Andreas Wetzel"
 __copyright__ = "Copyright 2011-2015, mindmade.org"
@@ -22,6 +24,9 @@ __email__ = "xbmc@mindmade.org"
 # constants definition
 ############################################
 PLUGINID = "plugin.video.teleboy"
+
+addon = xbmcaddon.Addon()
+LOGFILE = os.path.join(addon.getAddonInfo('path'), "resources", "log.txt")
 
 MODE_RECORDINGS = "recordings"
 MODE_PLAY = "play"
@@ -238,12 +243,66 @@ def play_url(url, title, img=""):
 
     xbmc.Player().play(url, li)
 
+# Tools
+
+
+def log(msg):
+    msg = msg.encode("latin-1")
+    logf = open(LOGFILE, "a")
+    logf.write("%s: " % datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S"))
+    logf.write(msg)
+    logf.write('\n')
+    logf.close()
+    xbmc.log("### %s" % msg, level=xbmc.LOGNOTICE)
+
+
+def notify(title, message):
+    xbmc.executebuiltin("XBMC.Notification(" + title + "," + message + ")")
+
+
+entitydict = {"E4": u"\xE4", "F6": u"\xF6", "FC": u"\xFC",
+              "C4": u"\xE4", "D6": u"\xF6", "DC": u"\xDC",
+              "2013": u"\u2013"}
+
+
+def htmldecode(s):
+    try:
+        h = HTMLParser.HTMLParser()
+        s = h.unescape(s)
+        for k in entitydict.keys():
+            s = s.replace("&#x" + k + ";", entitydict[k])
+    except UnicodeDecodeError:
+        pass
+
+    return s
+
+
+def fetchHttp(url, args={}, hdrs={}, post=False):
+    log("fetchHttp(%s): %s" % ("POST" if post else "GET", url))
+    if args:
+        log("args-keys: %s" % args.keys())
+    hdrs["User-Agent"] = "Mozilla/5.0 (X11; Linux i686; rv:5.0) Gecko/20100101 Firefox/5.0"  # noqa: E501
+    if post:
+        req = urllib2.Request(url, urllib.urlencode(args), hdrs)
+    else:
+        url = url + "?" + urllib.urlencode(args)
+        req = urllib2.Request(url, None, hdrs)
+    response = urllib2.urlopen(req)
+    encoding = re.findall(
+        "charset=([a-zA-Z0-9\-]+)", response.headers['content-type'])
+    text = response.read()
+    if len(encoding):
+        responsetext = unicode(text, encoding[0])  # noqa: F821
+    else:
+        responsetext = text
+    response.close()
+
+    return responsetext
+
 
 #
 # xbmc entry point
 ############################################
-sayHi()
-
 params = parameters_string_to_dict(sys.argv[2])
 mode = params.get(PARAMETER_KEY_MODE, "0")
 
