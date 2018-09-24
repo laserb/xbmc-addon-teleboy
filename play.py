9676511ff9b5531dee41192378674fd845577038
Raphael Freudiger <laser_b@gmx.ch>
2018-09-24 16:14:25 +0200

    feat(play): resolve url and let kodi resume items

--- play.py
+++ play.py
@@ -1,15 +1,16 @@
 import xbmcgui
-import xbmc
+import xbmcplugin
+
+from common import pluginhandle
 
 
 THUMBNAIL_URL = "https://media.service.teleboy.ch/media/teleboyteaser8/{}.jpg"
 
 
-def play_url(url, title, img="", start_percent=0):
-    li = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
+def play_url(url, title, start_percent=0):
+    li = xbmcgui.ListItem(title, path=url)
     li.setProperty("IsPlayable", "true")
     li.setProperty("Video", "true")
+#    li.setProperty("startPercent", str(start_percent))
 
-    li.setProperty("startPercent", str(start_percent))
-
-    xbmc.Player().play(url, li)
+    xbmcplugin.setResolvedUrl(pluginhandle, True, li)
